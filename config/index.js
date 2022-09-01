68446f0690ceaa0404a5026537315a7b0f17976e
1670642712 <112541716+1670642712@users.noreply.github.com>
2022-09-01 16:23:56 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,52 +2,33 @@ export const config = {
     /**
      * 公众号配置
      */
-
     // 公众号APP_ID
-    APP_ID: "",
-
+    APP_ID: "wx88f1ee0c4eb90d3f",
     // 公众号APP_SECRET
-    APP_SECRET: "",
-
+    APP_SECRET: "c41542c42cced4d2cd72eb74d700d7ea",
     // 模板消息id
-    TEMPLATE_ID: "xxxxxxxxxxxxx-y你懂的,填错狗头打烂VRMpu",
-
-    // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-    CALLBACK_TEMPLATE_ID: "xxxxxxxxxxxxx-y你懂的,填错狗头打烂VRMpu",
-
+    TEMPLATE_ID: "AYQa9n14k4USmolCDGopAlW-OPvuZf8PO1KdyI5C4FQ",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
     // [{
     //   name: "张三",
-    //   id: "wxafasdasdd-zxfvsdfd"
+    //   id: "ozJJv57X8VZdLPLZ453kLOTJZ0m8"
     // },
     // {
     //   name: "李四",
     //   id: "wxafasdasdd-zxfvsdfd"
     // }]
     USERS: [
-      {name: '老婆0', id: "1hxOO6VskYv0Kuf-y你懂的,填错狗头打烂VRMpuXhk"}, 
-      {name: '老婆1', id: "2hxOO6VskYv0Kuf-y你懂的,填错狗头打烂RMpuXhk"}, 
-      {name: '老婆2', id: "3hxOO6VskYv0Kuf-yw你懂的,填错狗头打烂2RMpuXhk"}, 
-      {name: '老婆3', id: "4hxOO6VskYv0Kuf-ywI你懂的,填错狗头打烂MpuXhk"}, 
-    ],
-    // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
-    CALLBACK_USERS: [
-      {name: '自己',id: "5hxOO6VskYv你懂的,填错狗头打烂-ywIZVRMpuXhk"}, 
-    ],
-     
+      {name: '老婆0', id: "ozJJv57X8VZdLPLZ453kLOTJZ0m8"}, 
+    ], 
     /**
      * 信息配置
      */
-
     /** 天气相关 */
-
     // 所在省份
     PROVINCE: "广东",
     // 所在城市
-    CITY: "惠州",
-
+    CITY: "深圳",
     /** 重要节日相关 */
-
     // 重要节日，修改名字为对应需要显示的名字, data 仅填月日即可, 请严格按照示例填写
     // tpye必须填！ 只能 “生日” 和 “节日” 二选一!
     // 生日时，name填写想要展示的名字，你可以填“美丽可爱亲亲老婆”
@@ -55,16 +36,13 @@ export const config = {
     FESTIVALS: [
       {"type": "生日", "name": "老婆", "year": "1996", "date": "01-01"},
       {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "01-04"},
-      {"type": "生日", "name": "李四", "year": "1996", "date": "08-29"},
       {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "08-29"},
     ],
     // 限制重要节日的展示条目, 需要填写数字; 
     // 如果为3, 则仅展示“将要到达” 的3个重要节日提醒，剩下的将被忽略
     // 如果为0, 则默认展示全部
-    FESTIVALS_LIMIT: 3,
-
+    FESTIVALS_LIMIT: 0,
     /** 日期相关 */
-
     // 在一起的日子，格式同上
     LOVE_DATE: "2015-05-01",
     // 结婚纪念日
